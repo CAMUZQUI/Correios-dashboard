664523b26491f974ccb98106da822b3870d1f0c3
CAMUZQUI <96161935+CAMUZQUI@users.noreply.github.com>
2024-02-27 09:38:54 -0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -467,3 +467,15 @@ document.getElementById('input-csv').addEventListener('change', function(e) {
     reader.readAsText(e.target.files[0]);
 });
 */
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  var cookies = document.cookie.split(";");
+
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i];
+    var eqPos = cookie.indexOf("=");
+    var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+  }
+});
+
